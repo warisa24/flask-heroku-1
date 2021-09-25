341c75f0775de870781d3fa4615353bb576fc3f1
warisa24 <91365608+warisa24@users.noreply.github.com>
2021-09-25 15:09:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -29,5 +29,9 @@ def hello():
 def get_api():
     return jsonify(data)
 
+@app.route('/name')
+def name():
+        return "วริษา โชคเฉลิม เลขที่ 24 ชั้น ม.4/10"
+
 if __name__ == "__main__":
     app.run(debug=False)
