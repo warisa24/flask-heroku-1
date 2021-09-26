40065919bf4943bc216c9a485aebea521fdac609
warisa24 <91365608+warisa24@users.noreply.github.com>
2021-09-26 13:03:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -62,4 +62,4 @@ def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
